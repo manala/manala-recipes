db0bca01d7d08da13902d3cd063ff65b9b802949
nervo <nervo@nervo.net>
2020-01-23 09:32:24 +0100

    [Elao - App] Merge text related makefiles

--- elao.app/.manala/make/make.text.mk
+++ elao.app/.manala/make/make.text.mk
@@ -1,3 +1,13 @@
+##########
+# Colors #
+##########
+
+COLOR_RESET   := \033[0m
+COLOR_ERROR   := \033[31m
+COLOR_INFO    := \033[32m
+COLOR_WARNING := \033[33m
+COLOR_COMMENT := \033[36m
+
 ###########
 # Message #
 ###########
