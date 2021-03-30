0e67f722f3ae6a1f9029c6814cacfe01a5d3d982
Florian Rey <florian.rey@elao.com>
2021-03-30 23:53:54 +0200

    [Lazy] Pass docker compose parameters from runtime to files

--- lazy.kubernetes/.manala/make/docker_compose.mk
+++ lazy.kubernetes/.manala/make/docker_compose.mk
@@ -8,15 +8,12 @@
 ifneq ($(container),docker)
 define docker_compose_run
 	docker-compose \
-		$(if $(_DOCKER_COMPOSE_FILE),--file $(_DOCKER_COMPOSE_FILE)) \
 		$(if $(_DOCKER_PROJECT_NAME),--project-name $(_DOCKER_PROJECT_NAME)) \
+		$(foreach file, $(_DOCKER_COMPOSE_FILES), \
+			--file $(file) \
+		) \
 		run \
 		--rm \
-		$(if $(OS_DARWIN), \
-			--volume /run/host-services/ssh-auth.sock:/run/host-services/ssh-auth.sock:ro \
-			-e SSH_AUTH_SOCK=/run/host-services/ssh-auth.sock \
-		) \
-		$(foreach volume,$(_DOCKER_VOLUMES),--volume $(realpath $(_ROOT_DIR)/$(volume)):$(realpath $(_ROOT_DIR)/$(volume)):cached) \
 		$(_DOCKER_SERVICE)
 endef
 else
@@ -30,8 +27,10 @@ endif
 ifneq ($(container),docker)
 define docker_compose_clean
 	docker-compose \
-		$(if $(_DOCKER_COMPOSE_FILE),--file $(_DOCKER_COMPOSE_FILE)) \
 		$(if $(_DOCKER_PROJECT_NAME),--project-name $(_DOCKER_PROJECT_NAME)) \
+		$(foreach file, $(_DOCKER_COMPOSE_FILES), \
+			--file $(file) \
+		) \
 		down \
 		--rmi all \
 		--volumes \
