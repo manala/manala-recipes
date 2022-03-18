289262c2961daa1b998243d1ae225bed2c4ebe8a
Florian Rey <nervo@nervo.net>
2022-03-18 10:01:57 +0100

    [Elao - App - Docker] Start ssh-agent when using ssh key

--- elao.app.docker/.manala/docker/entrypoint.sh
+++ elao.app.docker/.manala/docker/entrypoint.sh
@@ -2,19 +2,20 @@
 
 set -e
 
-# If ssh-agent bind differs from sock, establish an unpriviliged relay
+# If ssh-agent bind differs from sock, establish an unprivileged relay
 if [ -n "${SSH_AUTH_SOCK}" ] && [ -n "${MANALA_SSH_AUTH_SOCK_BIND}" ] && [ "${SSH_AUTH_SOCK}" != "${MANALA_SSH_AUTH_SOCK_BIND}" ]; then
+  printf "Bind privileged ssh-agent socket\n"
   socat \
     UNIX-LISTEN:${SSH_AUTH_SOCK},fork,mode=777 \
     UNIX-CONNECT:${MANALA_SSH_AUTH_SOCK_BIND} &
 fi
 
 # Ssh key
-if [ -n "${MANALA_SSH_KEY}" ]; then
-  install --directory --group app --owner app --mode 700 /home/app/.ssh
-  printenv MANALA_SSH_KEY > /home/app/.ssh/id_rsa
-  chown app:app /home/app/.ssh/id_rsa
-  chmod 600 /home/app/.ssh/id_rsa
+if [ -n "${SSH_AUTH_SOCK}" ] && [ -n "${MANALA_SSH_KEY}" ]; then
+  printf "Start ssh-agent as app user\n"
+  su app -c "ssh-agent -a ${SSH_AUTH_SOCK}"
+  printf "Add ssh key\n"
+  su app -c "echo \"${MANALA_SSH_KEY}\" | ssh-add -"
 fi
 
 exec "$@"
