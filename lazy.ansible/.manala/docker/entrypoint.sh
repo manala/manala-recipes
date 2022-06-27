05e729a097452c3d37c1ab3d469854dc839d178f
Florian Rey <nervo@nervo.net>
2022-06-27 13:57:19 +0200

    [Lazy] Ssh agent sock tunnel

--- lazy.ansible/.manala/docker/entrypoint.sh
+++ lazy.ansible/.manala/docker/entrypoint.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-# Fix docker for mac SSH_AUTH_SOCK permission
-if [ "${SSH_AUTH_SOCK}" = "/run/host-services/ssh-auth.sock" ] && [ -S "${SSH_AUTH_SOCK}" ]; then
-    chmod 777 "${SSH_AUTH_SOCK}"
+# If ssh-agent bind differs from sock, establish an unprivileged relay
+if [ -n "${SSH_AUTH_SOCK}" ] && [ -n "${MANALA_SSH_AUTH_SOCK_BIND}" ] && [ "${SSH_AUTH_SOCK}" != "${MANALA_SSH_AUTH_SOCK_BIND}" ]; then
+  socat \
+    UNIX-LISTEN:"${SSH_AUTH_SOCK}",fork,mode=777 \
+    UNIX-CONNECT:"${MANALA_SSH_AUTH_SOCK_BIND}" &
 fi
 
 # If docker bind differs from sock, establish an unprivileged relay
