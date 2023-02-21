5099af3351487fc021ef5f3ecb83184c246014aa
Florian Rey <nervo@nervo.net>
2023-02-21 10:47:23 +0100

    [All] Optimize makefile os detection helpers

--- lazy.ansible/.manala/make/os.mk
+++ lazy.ansible/.manala/make/os.mk
@@ -11,19 +11,11 @@
 #   echo $(if $(OS_LINUX),Running on Linux,*NOT* running on Linux)
 
 ifeq ($(OS),Windows_NT)
-OS := windows
+OS = windows
+OS_WINDOWS = 1
 else
-OS := $(shell uname | tr '[:upper:]' '[:lower:]')
-endif
-
-ifeq ($(OS),linux)
-OS_LINUX := 1
-endif
-
-ifeq ($(OS),darwin)
-OS_DARWIN := 1
-endif
-
-ifeq ($(OS),windows)
-OS_WINDOWS := 1
+# See: https://make.mad-scientist.net/deferred-simple-variable-expansion/
+OS = $(eval OS := $$(shell uname -s | tr '[:upper:]' '[:lower:]'))$(OS)
+OS_LINUX = $(if $(findstring linux,$(OS)),1)
+OS_DARWIN = $(if $(findstring darwin,$(OS)),1)
 endif
