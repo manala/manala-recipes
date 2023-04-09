f7ebd047a4c3edb4a370baadff71d9e449e4883a
Florian Rey <nervo@nervo.net>
2023-04-09 19:01:34 +0200

    Tests github actions

---
+++ lazy.symfony/.manala/github/system/setup/action.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const dir = core.getInput('dir');
+  console.log(`Hello ${dir}!`);
+  
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
