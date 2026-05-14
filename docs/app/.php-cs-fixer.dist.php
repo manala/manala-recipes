6ad23a89cfa23e7ecdbadb4ab1b3c47600d0de49
Thomas Jarrand <thomas@wecandoo.com>
2026-05-14 11:29:24 +0200

    chore: fix review feedbacks

--- docs/app/.php-cs-fixer.dist.php
+++ docs/app/.php-cs-fixer.dist.php
@@ -4,6 +4,7 @@ $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
     ->exclude('var')
     ->exclude('node_modules')
+    ->notPath('config/reference.php')
 ;
 
 return (new PhpCsFixer\Config())
